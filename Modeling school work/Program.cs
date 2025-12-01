b4b42335dca4978e47d86294b8bae20fec93808c
StanislavSavelyev <texnikum2288@mail.ru>
2025-12-02 00:33:49 +0500

    Добавил реализацию в мейн и метод который выводит список учеников

--- Modeling school work/Program.cs
+++ Modeling school work/Program.cs
@@ -18,16 +18,45 @@ namespace Modeling_school_work
         public class School
         {
             public string Name { get; set; }
-            public List<Student> Students { get; set; } = new List<Student>();
-            public School(string name, Student student)
+            public List<Student> Students { get; set; }
+            public School(string name)
             {
                 Name = name;
-                Students.Add(student);
+                Students = new List<Student>();
+            }
+
+            public void PrintStudents()
+            {
+                if (Students.Count == 0)
+                {
+                    Console.WriteLine($"В школе {Name} пока что нету учеников");
+                }
+                else
+                {
+                    foreach (Student student in Students)
+                    {
+                        Console.WriteLine("{0, -10} {1, -10}", student.FistName, student.LastName);
+                    }
+
+                }
             }
         }
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Здравствуйте, введите название школы.");
+            string SchoolName = Console.ReadLine();
+            var School = new School(SchoolName);
+            Console.WriteLine($"Школа {School.Name} успешно создана.");
+            while (true)
+            {
+                Console.WriteLine($"Хотите посмотреть список всех студентов школы {School.Name}? Введите Да или Нет");
+                string userAnswer = Console.ReadLine().ToLower();
+                if (userAnswer == "да")
+                {
+                    School.PrintStudents();
+                }
+            }
         }
     }
 }
