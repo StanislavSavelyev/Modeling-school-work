f3a10bd9d846e85339e13c9e7970393a2c166f09
StanislavSavelyev <texnikum2288@mail.ru>
2025-12-01 22:54:21 +0500

    Создал класс для студента.

--- Modeling school work/Program.cs
+++ Modeling school work/Program.cs
@@ -1,7 +1,21 @@
-﻿namespace Modeling_school_work
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Modeling_school_work
 {
     internal class Program
     {
+
+        public class Student
+        {
+            public string FistName { get; set; }
+            public string LastName { get; set; }
+            public Student(string firstName, string lastName)
+            {
+                firstName = firstName;
+                LastName = lastName;
+            }
+        }
+
         static void Main(string[] args)
         {
         }
